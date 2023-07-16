b46008bca461adee099d2b218ef2b85e2fea433d
tomyprs <tomiprs25@gmail.com>
2023-07-16 19:40:22 +0700

    Add AFK module
    - Add a new AFK class in caligo/modules/afk.py to handle AFK functionality
    - Implement methods to set and retrieve AFK status, handle incoming and outgoing messages, and respond to the AFK command
    
    Signed-off-by: tomyprs <tomiprs25@gmail.com>

---
+++ caligo/modules/afk.py
@@ -0,0 +1,89 @@
+import asyncio
+from datetime import datetime, timedelta
+from typing import Any, ClassVar, Dict, Optional
+
+from pyrogram import filters, types
+
+from caligo import command, listener, module
+from caligo.core import database
+from caligo.util.cache_limiter import CacheLimiter
+from caligo.util.time import format_duration_td
+
+
+class AFK(module.Module):
+    name: ClassVar[str] = "afk"
+
+    db: database.AsyncCollection
+    cache: CacheLimiter
+
+    async def on_load(self) -> None:
+        self.db = self.bot.db[self.name.upper()]
+        self.cache = CacheLimiter(ttl=60, max_value=3)
+
+    async def get_afk_setting(self) -> Optional[Dict[str, any]]:
+        """Query the database to retrieve the AFK setting."""
+        return await self.db.find_one({"_id": 0})
+
+    async def get_afk_duration(self) -> timedelta:
+        """Retrieve the AFK duration."""
+        setting = await self.get_afk_setting()
+        if setting and "time" in setting:
+            duration = datetime.now() - setting["time"]
+            return duration
+        else:
+            return timedelta(0)
+
+    async def set_afk(self, afk: bool = False, reason: str = None) -> bool:
+        """Update the AFK setting in the database."""
+        afk_setting = {"afk_setting": afk, "time": datetime.now()}
+        if reason is not None:
+            afk_setting["reason"] = reason
+        await self.db.update_one({"_id": 0}, {"$set": afk_setting}, upsert=True)
+        return afk
+
+    @listener.filters(filters.outgoing)
+    async def on_message(self, message: types.Message) -> None:
+        afk = await self.get_afk_setting()
+        if afk and afk.get("afk_setting", False):
+            await self.set_afk(False)
+            rest = await message.reply("__You are no longer AFK!__")
+            await asyncio.sleep(5)
+            await rest.delete()
+
+    @listener.filters(
+        ~filters.bot & ~filters.channel & ~filters.service & filters.private
+        | filters.mentioned
+    )
+    async def on_message(self, ctx: command.Context, message: types.Message) -> None:
+        # sanity check
+        if message.from_user is None or message.from_user.is_bot:
+            return
+        user_id = message.from_user.id
+        if await self.cache.exceeded(user_id):
+            # User has exceeded rate limit, do not send AFK message
+            return
+        else:
+            # User has not exceeded rate limit, increment the rate limit and send AFK message
+            await self.cache.increment(user_id)
+            afk = await self.get_afk_setting()
+            if afk and afk.get("afk_setting", False):
+                if user_id != self.bot.uid:
+                    reason = afk.get("reason", "No reason provided")
+                    duration = format_duration_td(await self.get_afk_duration())
+                    reply_text = f"__I'm currently AFK!__\n**Duration:** `{duration}`"
+                    if reason:
+                        reply_text += f"\n**Reason:** `{reason}`"
+                    return await ctx.respond(reply_text, delete_after=10)
+
+    @command.desc("Set your AFK status")
+    @command.alias("brb")
+    @command.usage("afk [reason] or leave it empty", optional=True)
+    async def cmd_afk(self, ctx: command.Context) -> str:
+        afk = await self.get_afk_setting()
+        if afk and afk.get("afk_setting", False):
+            await self.set_afk(False, reason=False)
+            return await ctx.respond("__You are no longer AFK!__", delete_after=5)
+        else:
+            reason = ctx.input if ctx.input else None
+            await self.set_afk(True, reason)
+            return await ctx.respond("__You are now AFK!__", delete_after=5)
